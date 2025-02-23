af6de4b0422a66d69429b09a26f884d6a17c82e8
H3rnanG <h3rnangamarr@gmail.com>
2025-02-23 11:39:59 -0500

    feat: fzf-lua

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,15 @@
+return {
+  "ibhagwan/fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {},
+  keys = {
+    -- Files
+    {'<leader>ff', ':FzfLua files<cr>'},
+    {'<leader>fb', ':FzfLua buffers<cr>'},
+    {'<leader>fh', ':FzfLua oldfiles<cr>'},
+
+    -- Search
+    {'<leader>fg', ':FzfLua live_grep_native<cr>'},
+    {'<leader>fc', ':FzfLua lgrep_curbuf<cr>'},
+  }
+}
