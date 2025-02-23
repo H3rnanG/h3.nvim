8ddd78fa6223ffa9185443f8007d4d4efb52ff25
H3rnanG <h3rnangamarr@gmail.com>
2025-02-22 19:41:34 -0500

    feat: Install lazy and add basic configuration

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -2,8 +2,10 @@ local g = vim.g
 local o = vim.o
 local opt = vim.opt
 
-# Sangria
 opt.tabstop = 2
 opt.smartindent = true
 opt.shiftwidth = 2
 opt.expandtab = true
+
+opt.number = true
+opt.relativenumber = true
