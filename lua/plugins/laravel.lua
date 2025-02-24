29c863c1cfacb322a3e6c4bfb25d5e8bdfc7cc03
H3rnanG <h3rnangamarr@gmail.com>
2025-02-23 20:05:13 -0500

    feat: laravel

---
+++ lua/plugins/laravel.lua
@@ -0,0 +1,39 @@
+return {
+  'adalessa/laravel.nvim',
+  dependencies = {
+    'tpope/vim-dotenv',
+    'nvim-telescope/telescope.nvim',
+    'MunifTanjim/nui.nvim',
+    'kevinhwang91/promise-async',
+  },
+  cmd = { 'Laravel' },
+  keys = {
+    { 
+      '<leader>la', 
+      mode = 'n', 
+      ':Laravel artisan<cr>',
+      desc = 'Laravel artisan'
+    },
+    { 
+      '<leader>lr', 
+      mode = 'n', 
+      ':Laravel routes<cr>',
+      desc = 'Laravel routes'
+    },
+    { 
+      '<leader>lm', 
+      mode = 'n', 
+      ':Laravel related<cr>',
+      desc = 'Laravel related'
+    },
+    { 
+      '<leader>lh', 
+      mode = 'n', 
+      ':Laravel history<cr>',
+      desc = 'Laravel history'
+    },
+  },
+  event = { 'VeryLazy' },
+  opts = {},
+  config = true,
+}
