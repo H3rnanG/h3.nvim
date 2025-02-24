72cc2c64813d53e8bba9adf60132512661ad0309
H3rnanG <h3rnangamarr@gmail.com>
2025-02-23 20:02:58 -0500

    feat: yazi

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
 require('config.settings')
 require('config.keymaps')
 require('config.lazy')
+
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
