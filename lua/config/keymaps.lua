c925267dc943173343c5cb5e510fda5705e152e1
H3rnanG <h3rnangamarr@gmail.com>
2025-02-26 07:42:45 -0500

    feat: more keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,16 @@ km.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 km.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 km.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 km.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+km.set('n', '+', '<C-a>')
+km.set('n', '-', '<C-x>')
+
+km.set('n', '<C-a>', 'gg<S-v>G')
+
+km.set('n', '<leader>sh', ':split<cr><C-w>j')
+km.set('n', '<leader>sv', ':vsplit<cr><C-W>l')
+
+km.set('n', '<C-w>h', '<C-w><')
+km.set('n', '<C-w>l', '<C-w>>')
+km.set('n', '<C-w>k', '<C-w>+')
+km.set('n', '<C-w>j', '<C-w>-')
