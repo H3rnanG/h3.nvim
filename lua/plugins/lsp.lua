6e4ad3e001b097a728e1d88c907212ed9aaaaa88
H3rnanG <h3rnangamarr@gmail.com>
2025-02-26 07:46:47 -0500

    refactor: lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,24 +1,24 @@
 return {
-  "neovim/nvim-lspconfig",
+  'neovim/nvim-lspconfig',
   dependencies = {
-    "williamboman/mason-lspconfig.nvim"
+    'williamboman/mason-lspconfig.nvim'
   },
   config = function()
-    require("mason-lspconfig").setup({})
-    local lspconfig = require("lspconfig")
+    require('mason-lspconfig').setup({})
+    local lspconfig = require('lspconfig')
 
-    require("mason-lspconfig").setup_handlers({
+    require('mason-lspconfig').setup_handlers({
       function(server_name)
         lspconfig[server_name].setup({})
       end,
 
-      ["lua_ls"] = function()
+      ['lua_ls'] = function()
         lspconfig.lua_ls.setup({
           settings = {
             Lua = {
               diagnostics = {
                 globals = {
-                  "vim"
+                  'vim'
                 }
               }
             }
