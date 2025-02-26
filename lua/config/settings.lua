7bd5dffeaea3af5533bc5668a758c920a4abde96
H3rnanG <h3rnangamarr@gmail.com>
2025-02-26 07:46:30 -0500

    feat: settings

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,14 +1,26 @@
 local g = vim.g
-local o = vim.o
+--local o = vim.o
 local opt = vim.opt
-local api = vim.api
+--local api = vim.api
 
+vim.scriptencoding = 'utf8'
+opt.encoding = 'utf8'
+opt.fileencoding = 'utf8'
 opt.tabstop = 2
 opt.smartindent = true
 opt.shiftwidth = 2
 opt.expandtab = true
-
 opt.number = true
 opt.relativenumber = true
+opt.signcolumn = 'yes'
+opt.ai = true
+opt.si = true
 
 g.mapleader = " "
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = true,
+  float = { source = "always" }
+})
