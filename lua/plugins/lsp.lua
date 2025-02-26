c6b80a376d1ef27a5069ce09621aaa351dedb6ba
H3rnanG <h3rnangamarr@gmail.com>
2025-02-26 08:10:33 -0500

    feat: prettier

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,19 @@
 return {
   'neovim/nvim-lspconfig',
   dependencies = {
-    'williamboman/mason-lspconfig.nvim'
+    'williamboman/mason-lspconfig.nvim',
+    'nvimtools/none-ls.nvim',
   },
   config = function()
     require('mason-lspconfig').setup({})
     local lspconfig = require('lspconfig')
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.prettier,
+      },
+    })
 
     require('mason-lspconfig').setup_handlers({
       function(server_name)
@@ -24,6 +32,13 @@ return {
             }
           }
         })
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function()
+        vim.lsp.buf.format({ async = false })
       end,
     })
   end
