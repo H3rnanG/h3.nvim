0f93bdcdf6d780269f3bc17186ad2f08caa69e42
H3rnanG <h3rnangamarr@gmail.com>
2025-03-04 13:38:21 -0500

    refactor: fzf with prettier

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,15 +1,19 @@
 return {
   'ibhagwan/fzf-lua',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  event = 'VimEnter',
   opts = {},
   keys = {
     -- Files
-    {'<leader>ff', ':FzfLua files<cr>'},
-    {'<leader>fb', ':FzfLua buffers<cr>'},
-    {'<leader>fh', ':FzfLua oldfiles<cr>'},
+    { '<leader>ff', ':FzfLua files<cr>' },
+    { '<leader>fb', ':FzfLua buffers<cr>' },
+    { '<leader>fh', ':FzfLua oldfiles<cr>' },
 
     -- Search
-    {'<leader>fg', ':FzfLua live_grep_native<cr>'},
-    {'<leader>fc', ':FzfLua lgrep_curbuf<cr>'},
+    { '<leader>fg', ':FzfLua live_grep_native<cr>' },
+    { '<leader>fc', ':FzfLua lgrep_curbuf<cr>' },
+
+    -- Lsp
+    { 'gd',         ':FzfLua lsp_definitions<cr>' },
   }
 }
